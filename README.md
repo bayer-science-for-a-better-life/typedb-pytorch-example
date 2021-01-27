1f11899596f8335280ed127c377ffa087fec25d9
Joren Retel <joren.retel@bayer.com>
2021-01-27 22:31:31 +0100

    Moving around stuff.

---
+++ README.md
@@ -0,0 +1,75 @@
+Very Minimal Grakn + Pytorch Geometric Example
+==============================================
+
+Copied directly from the diagnosis example in kglib and importing
+as much code as possible from there. 
+
+Discaimer: machine learning makes no sense now. For now the graph convolution
+done and loss is not the same one at all. We can change this to be
+more like the original graphnets example. However, it was more about
+exploring the code and seeing which part of kglib would potentially
+be nice to have decoupled from tensorflow and graphnets.
+
+For now there are only two files here:
+
+* diagnosis.py (start here. Contains Dataloader, Model and Training loop)
+* transforms (some functions to transform the netwokx graph in kglib to
+  Pytorch Geometric Data object)
+
+## Environment
+Here I used conda to create an environment. To recreate my setup
+clone the following 3 repositories and run create the environment
+using the environment-local.yml file:
+
+```
+git clone https://github.com/graknlabs/kglib.git
+git clone https://github.com/jorenretel/grakn-pytorch-geometric.git
+git clone https://github.com/jorenretel/grakn-pytorch-example.git
+
+cd grakn-pytorch-example
+
+conda env create -f environment-local.yml
+```
+
+"grakn-pytorch-geometric" is a separate repo because I did not want to mix
+library and example code.
+
+I was not able to install grakn-client and grakn-kglib both from pypi
+because of conflicting dependencies. I have kglib "installed" in editable
+mode (by cloning from github and adding a setup.py) without the
+dependencies like tensorflow installed. You probably have it installed though.
+If not this is the setup.py I added to the kglib directory:
+
+```
+from setuptools import setup
+
+setup(
+    name='kglib',
+    version='',
+    packages=['kglib'],
+    url='',
+    license='',
+    author='grakn',
+    author_email='',
+    description=''
+)
+```
+
+## Run the example
+
+Activate enviroment:
+```
+conda activate grakn-pytorch-geometric
+```
+
+Prepare the grakn database (only once):
+```
+python populate_database/prepare.py
+```
+
+Run example:
+
+```
+python diagnosis.py
+```
+
--- diagnosis.py
+++ diagnosis.py
@@ -1,47 +1,60 @@
 import torch
 import torch.nn.functional as F
-
 from torch_geometric.data import DataLoader
 from torch_geometric.nn import GCNConv
-
-from grakn_pytorch_geometric.dataloader import GraknPytorchGeometricDataSet
-from queries import get_query_handles
+from kglib.kgcn.examples.diagnosis.diagnosis import get_query_handles
+from grakn_dataloading.data import GraknPytorchGeometricDataSet
 from transforms import networkx_transform
 
 
-def train(example_indices):
-    grakn_dataset = GraknPytorchGeometricDataSet(
-        example_indices=list(range(20)),
-        get_query_handles_for_id=get_query_handles,
-        infer=True,
-        uri="localhost:48555",
-        keyspace="diagnosis",
-        networkx_transform=networkx_transform
-    )
+example_indices = list(range(20))
 
+grakn_dataset = GraknPytorchGeometricDataSet(
+    example_indices=example_indices,
+    get_query_handles_for_id=get_query_handles,
+    infer=True,
+    uri="localhost:48555",
+    keyspace="diagnosis",
+    networkx_transform=networkx_transform,
+)
 
-    dataloader = DataLoader(grakn_dataset, batch_size=2)
+# TODO: multiple workers crashes
+dataloader = DataLoader(grakn_dataset, batch_size=2, num_workers=0, shuffle=True)
 
-    for data in dataloader:
-        print(data)
+loss_function = torch.nn.CrossEntropyLoss()
 
 
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = GCNConv(dataset.num_node_features, 16)
-        self.conv2 = GCNConv(16, dataset.num_classes)
+        self.conv1 = GCNConv(grakn_dataset.num_node_features, 16)
+        self.conv2 = GCNConv(16, 3)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
+        return x
 
-        return F.log_softmax(x, dim=1)
+
+model = Net()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+
+def train():
+
+    for epoch in range(20):
+        for i, data in enumerate(dataloader):
+            optimizer.zero_grad()
+            model.train()
+            prediction = model(data)
+            loss = loss_function(prediction, data.y)
+            print("epoch: {}, step: {}, loss: {}".format(epoch, i, loss))
+            loss.backward()
+
 
 if __name__ == "__main__":
-    example_inices = list(range(20))
-    train(example_inices)
+
+    train()
