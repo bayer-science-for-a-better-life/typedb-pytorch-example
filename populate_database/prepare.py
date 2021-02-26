860f8e2646d033bdd134a2759f6a981aba578a67
hkuich <henning.kuich@bayer.com>
2021-02-26 12:57:43 +0100

    cleaned up prepare - there are some changes to console and other logic to define and insert queries...

--- populate_database/prepare.py
+++ populate_database/prepare.py
@@ -6,24 +6,25 @@ from grakn.client import GraknClient
 from grakn.client import SessionType
 from grakn.client import TransactionType
 
+import subprocess
+
+DATABASENAME = "diagnosis"
+
+
 def main(number_of_examples=100):
+    subprocess.call(
+        ["/Users/henning.kuich@bayer.com/tools/grakn-core-all-mac-2.0.0-alpha-8/grakn", "console", "--script", "schema_load.script"]
+    )
 
     client = GraknClient.core("localhost:1729")
-    with open ("schema.gql", "r") as schema_file:
-        schema = schema_file.readlines()
-        schema_as_str = " ".join(schema)
-        with client.session("diagnosis", SessionType.SCHEMA) as session:
-            with session.transaction(TransactionType.WRITE) as txn:
-                txn.query().define(schema_as_str)
-                txn.commit()
 
     with open ("data.gql", "r") as data_file:
         data = data_file.readlines()
-        data_as_str = " ".join(data)
-        with client.session("diagnosis", SessionType.DATA) as session:
-            with session.transaction(TransactionType.WRITE) as txn:
-                txn.query().insert(data_as_str)
-                txn.commit()
+        with client.session(DATABASENAME, SessionType.DATA) as session:
+            for line in data:
+                with session.transaction(TransactionType.WRITE) as txn:
+                    txn.query().insert(line)
+                    txn.commit()
 
     client.close()
     generate_example_graphs(number_of_examples)
---
+++ populate_database/test_insert.py
@@ -0,0 +1,25 @@
+from grakn.client import GraknClient
+from grakn.rpc.session import SessionType
+from grakn.rpc.transaction import TransactionType
+import json
+
+q = '''match $x isa {}; get $x; limit 5; '''
+
+def execute_query(query:str):
+    with GraknClient.core("localhost:1729") as client:
+        with client.session("diagnosis", SessionType.DATA) as session:
+            with session.transaction(TransactionType.READ) as txn:
+                answer_it = txn.query().match(query)
+                for answer in answer_it:
+                    print(json.dumps(answer, indent=2, default=str))
+                print("-----")
+
+
+print("person: ")
+execute_query(q.format("person"))
+print("symptom: ")
+execute_query(q.format("symptom"))
+print("disease: ")
+execute_query(q.format("disease"))
+print("diagnosis: ")
+execute_query(q.format("diagnosis"))
