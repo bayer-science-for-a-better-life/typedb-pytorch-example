2c896f652eac5afd610e4aa3afd9ddc907bdbf18
Joren Retel <joren.retel@bayer.com>
2021-03-11 22:14:11 +0100

    Adding visualization code.

---
+++ visualize_samples.py
@@ -0,0 +1,18 @@
+from kglib.kgcn_data_loader.dataset.grakn_networkx_dataset import GraknNetworkxDataSet
+from about_this_graph import get_query_handles
+
+from graph_dash_app.app import create_app
+
+dataset = GraknNetworkxDataSet(
+    example_indices=list(range(100)),
+    get_query_handles_for_id=get_query_handles,
+    database="diagnosis",
+    uri="localhost:1729",
+    infer=True,
+    transform=None,
+)
+
+
+
+if __name__ == '__main__':
+    create_app(dataset).run_server(debug=True)
